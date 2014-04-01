f90f38fbd9d434abbe86dab64b16eaed9080976a
hugoduar <hugoduar@hotmail.com Default Deletehugoduar@hotmail.com>
2014-04-01 01:50:23 -0600

    desde fabric

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,6 @@ def deploy():
  print("enter your git commit comment: ")
  comment = raw_input()
  local('git commit -m "%s"' % comment)
- local('git push -u origin master')
  local('heroku maintenance:on')
  local('git push heroku master')
  local('heroku maintenance:off')
