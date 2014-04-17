6feab802cbeb3885dc85546d05af859969e0e5c2
hugoduar <hugoduar@hotmail.com Default Deletehugoduar@hotmail.com>
2014-04-17 01:24:36 -0500

    fix os import

--- diloo/settings.py
+++ diloo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# import os
+import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 import dj_database_url
 
