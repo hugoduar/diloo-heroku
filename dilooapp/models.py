547979f5fcdae5eeaaef8220d87bd0907874107c
hugoduar <hugoduar@hotmail.com Default Deletehugoduar@hotmail.com>
2014-04-18 02:11:19 -0500

    migrations

--- dilooapp/models.py
+++ dilooapp/models.py
@@ -4,64 +4,76 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Critic(models.Model):
-	user = models.OneToOneField(User)
-	display_name = models.CharField(max_length=50)
-	readers = models.ManyToManyField("self", blank=True)
-	to_read = models.ManyToManyField("self", blank=True)
-	image = models.ImageField(upload_to = 'media/images/profile/', default = 'media/images/profile/no-img.jpg')
-	def __unicode__(self):
-		return self.user.username
+    user = models.OneToOneField(User)
+    display_name = models.CharField(max_length=50)
+    readers = models.ManyToManyField("self", blank=True)
+    to_read = models.ManyToManyField("self", blank=True)
+    image = models.ImageField(
+        upload_to='media/images/profile/', default='media/images/profile/no-img.jpg')
+
+    def __unicode__(self):
+        return self.user.username
+
 
 class Category(models.Model):
-	name = models.CharField(max_length=50)
-	def __unicode__(self):
-		return self.name
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
 
 class Thing(models.Model):
-	category = models.ForeignKey(Category)
-	critic = models.ForeignKey(Critic)
-	name = models.CharField(max_length=140)
-	description = models.CharField(max_length=500)
-	dateCreated = models.DateTimeField('pub_date')
-	def __unicode__(self):
-		return self.name
+    category = models.ForeignKey(Category)
+    critic = models.ForeignKey(Critic)
+    name = models.CharField(max_length=140)
+    description = models.CharField(max_length=500)
+    dateCreated = models.DateTimeField('pub_date')
+
+    def __unicode__(self):
+        return self.name
+
 
 class Topic(models.Model):
-	name = models.CharField(max_length=100)
-	def __unicode__(self):
-		return self.name
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
 
 class Review(models.Model):
-	content = models.CharField(max_length=1000)
-	topics = models.ManyToManyField(Topic, blank=True)
-	thing = models.ForeignKey(Thing)
-	critic = models.ForeignKey(Critic)
-	avg = models.FloatField()
-	readings = models.IntegerField()
-	def __unicode__(self):
-		return self.thing.name + ' - ' +self.content
+    content = models.CharField(max_length=1000)
+    topics = models.ManyToManyField(Topic, blank=True)
+    thing = models.ForeignKey(Thing)
+    critic = models.ForeignKey(Critic)
+    avg = models.FloatField()
+    readings = models.IntegerField()
+
+    def __unicode__(self):
+        return self.thing.name + ' - ' + self.content
+
 
 class Comment(models.Model):
-	content = models.CharField(max_length=1000)
-	review = models.ForeignKey(Review)
-	critic = models.ForeignKey(Critic)
-	def __unicode__(self):
-		return self.critic.username
+    content = models.CharField(max_length=1000)
+    review = models.ForeignKey(Review)
+    critic = models.ForeignKey(Critic)
+
+    def __unicode__(self):
+        return self.critic.username
+
 
 class Note(models.Model):
-	GOOD = 3
-	REGULAR = 2
-	BAD = 1
+    GOOD = 3
+    REGULAR = 2
+    BAD = 1
 
-	NOTE_CHOICE = (
-			(GOOD, 3),
-			(REGULAR, 2),
-			(BAD, 1)
-		)
-	note = models.IntegerField(choices=NOTE_CHOICE)
-	critic = models.ForeignKey(Critic)
-	review = models.ForeignKey(Review)
-	def __unicode__(self):
-		return self.critic.user.username + ' - ' + str(self.note)
+    NOTE_CHOICE = (
+        (GOOD, 3),
+        (REGULAR, 2),
+        (BAD, 1)
+    )
+    note = models.IntegerField(choices=NOTE_CHOICE)
+    critic = models.ForeignKey(Critic)
+    review = models.ForeignKey(Review)
 
-
+    def __unicode__(self):
+        return self.critic.user.username + ' - ' + str(self.note)
--- dilooapp/views.py
+++ dilooapp/views.py
@@ -13,70 +13,74 @@ from dilooapp.models import Category, Thing, Review
 from dilooapp.forms import UserForm
 # Create your views here.
 
+
 def base(request):
-	user = request.user
-	return render_to_response("base.html", {'user': user})
+    user = request.user
+    return render_to_response("base.html", {'user': user})
+
+
 def home(request):
-	context = RequestContext(request)
-	categories = Category.objects.all()
-	things = Thing.objects.all().order_by('-dateCreated')
-	reviews = Review.objects.all()
-	return render_to_response("index.html", {'categories': categories, 'things': things, 'reviews': reviews, 'user': request.user}, context)
+    context = RequestContext(request)
+    categories = Category.objects.all()
+    things = Thing.objects.all().order_by('-dateCreated')
+    reviews = Review.objects.all()
+    return render_to_response("index.html", {'categories': categories, 'things': things, 'reviews': reviews, 'user': request.user}, context)
 
 
 def category(request, id_category):
-	categories = Category.objects.all()
-	things = Thing.objects.filter(category=id_category)
-	return render_to_response("index.html", {'categories': categories, 'things': things, 'user': request.user}) 
+    categories = Category.objects.all()
+    things = Thing.objects.filter(category=id_category)
+    return render_to_response("index.html", {'categories': categories, 'things': things, 'user': request.user})
+
 
 def index(request):
-	context = RequestContext(request)
-	return render_to_response("login_page.html", {'hola': 'mundo', 'username': request.user}, context)
+    context = RequestContext(request)
+    return render_to_response("login_page.html", {'hola': 'mundo', 'username': request.user}, context)
+
 
 def login(request):
-	context = RequestContext(request)
-	exists = True
-	paso = 'No ha pasado: ' + request.user.username
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			if user.is_active:
-				auth_login(request, user)
-				exists = True
-				paso = 'paso: ' + request.user.username
-			else:
-				pass	
-		else: 
-			pass
-	else:
-		pass
-	return render_to_response("login_page.html", {'ya':paso, 'exist': exists, 'user': user}, context)
+    context = RequestContext(request)
+    user = None
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                auth_login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                pass
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+    elif request.method == 'GET' and user is None: 
+		return HttpResponseRedirect('/')
+    else:
+    	return render_to_response("login_page.html", {}, context)
+    
+
 
 def show(request, numero):
-	n = request.GET['p']
-	return HttpResponse("Tu numero es:" +numero + n)
+    n = request.GET['p']
+    return HttpResponse("Tu numero es:" + numero + n)
+
 
 def logout(request):
-	context = RequestContext(request)
-	auth_logout(request)
-	return HttpResponseRedirect('/login/')
-
-def register(request):
-	context = RequestContext(request)
-	error = True
-	if request.method == 'POST':
-		form = UserForm(data=request.POST)
-		if form.is_valid():
-			form.save()
-			error = False
-	else:
-		form = UserForm()
-	
-	return render_to_response("registrar.html", {'form': form, 'error': error}, context)
+    context = RequestContext(request)
+    auth_logout(request)
+    return HttpResponseRedirect('/')
 
 
+# def register(request):
+# 	context = RequestContext(request)
+#     error = True
+#     if request.method == 'POST':
+#     	form = UserForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             error = False
+#     else:
+#         form = UserForm()
 
-
-
+#     return render_to_response("registrar.html", {'form': form, 'error': error}, context)
