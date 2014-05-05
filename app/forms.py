b719279c026664de84205e42b1365c98d3d966ef
hugoduar <hugoduar@hotmail.com Default Deletehugoduar@hotmail.com>
2014-05-05 02:30:49 -0500

    users system

--- app/forms.py
+++ app/forms.py
@@ -6,63 +6,60 @@ from app.models import Critic
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 class UserForm(forms.ModelForm):
-   	error_messages = {
+    error_messages = {
         'duplicate_username': _("El nombre de usuario ya existe."),
         'password_mismatch': _("Las dos password no coincidieron."),
     }
-   	username = forms.RegexField(label=_("Username"), max_length=30,
+    username = forms.RegexField(label=_("Username"), max_length=30,
         regex=r'^[\w.@+-]+$',
         help_text=_("Menos de 30 caracteres"),
         error_messages={
             'invalid': _("Solo letras, numeros y "
                          "@/./+/-/_ caracteres.")})
-   	password1 = forms.CharField(label=_("Password"),
+    password1 = forms.CharField(label=_("Password"),
         widget=forms.PasswordInput)
-   	password2 = forms.CharField(label=_("Repetir password"),
+    password2 = forms.CharField(label=_("Repetir password"),
         widget=forms.PasswordInput,
         help_text=_("Introduce la misma password."))
 
-   	class Meta:
-   		model = User
-   		fields = ("username",)
+    class Meta:
+      model = User
+      fields = ("username", )
 
-   	def clean_username(self):
+    def clean_username(self):
      
-		# Since User.username is unique, this check is redundant,
+    # Since User.username is unique, this check is redundant,
      
-		# but it sets a nicer error message than the ORM. See #13147.
-		username = self.cleaned_data["username"]
+    # but it sets a nicer error message than the ORM. See #13147.
+    username = self.cleaned_data["username"]
      
-		try:
-			User._default_manager.get(username=username)
+    try:
+      User._default_manager.get(username=username)
      
-		except User.DoesNotExist:
-			return username
+    except User.DoesNotExist:
+      return username
      
-		raise forms.ValidationError(
+    raise forms.ValidationError(
             self.error_messages['duplicate_username'],
             code='duplicate_username',
         )
-	def clean_password2(self):
-		password1 = self.cleaned_data.get("password1")
-		password2 = self.cleaned_data.get("password2")
-		if password1 and password2 and password1 != password2:
-			raise forms.ValidationError(self.error_messages['password_mismatch'],code='password_mismatch',)
-		return password2
+  def clean_password2(self):
+    password1 = self.cleaned_data.get("password1")
+    password2 = self.cleaned_data.get("password2")
+    if password1 and password2 and password1 != password2:
+      raise forms.ValidationError(self.error_messages['password_mismatch'],code='password_mismatch',)
+    return password2
 
-	def save(self, commit=True):
-		user = super(UserForm, self).save(commit=False)
-		user.set_password(self.cleaned_data["password1"])
-		if commit:
-			user.save()
-		return user
-
+  def save(self, commit=True):
+    user = super(UserForm, self).save(commit=False)
+    user.set_password(self.cleaned_data["password1"])
+    if commit:
+      user.save()
+    return user
 
 class CriticForm(forms.ModelForm):
-	class Meta:
-   		model = Critic
-   		fields = ("user",) 
-
+  class Meta:
+      model = Critic
 
 
 
--- app/views.py
+++ app/views.py
@@ -25,16 +25,17 @@ def main(request):
         reviews = Review.objects.all()
         return render_to_response("index.html", {"reviews": reviews, "user_form":user_form, "critic_form":critic_form}, RequestContext(request)) 
 
-@login_required(login_url='/')	
+@login_required(login_url='/login') 
 def feed(request):
     context = RequestContext(request)
     categories = Category.objects.all()
     reviews = Review.objects.all()
     personal_reviews = Review.objects.all()
     if request.user.is_active:
-    	critic = Critic.objects.get(id=1)
-    	personal_reviews = Review.objects.all()
-    return render_to_response("feed.html", {'categories': categories, 'reviews': reviews, 'user': request.user, 'personal_reviews': personal_reviews}, context)
+        print request.user
+        critic = Critic.objects.get(user=request.user)
+        personal_reviews = Review.objects.all()
+    return render_to_response("feed.html", {'categories': categories, 'reviews': reviews, 'user': request.user, 'personal_reviews': personal_reviews, 'critic': critic}, context)
 
 
 def login(request):
@@ -47,16 +48,16 @@ def login(request):
         if user is not None:
             if user.is_active:
                 auth_login(request, user)
-                return HttpResponseRedirect('/')
+                return HttpResponseRedirect('/feed')
             else:
                 pass
         else:
             print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+            return render_to_response("login_page.html", {"error": True, "username": username}, context)
     elif request.method == 'GET' and user is None: 
-		return HttpResponseRedirect('/')
+        return render_to_response("login_page.html", {"error": False}, context)
     else:
-    	return render_to_response("login_page.html", {}, context)
+        return render_to_response("login_page.html", {"error": True}, context)
 
 
 def logout(request):
@@ -66,13 +67,30 @@ def logout(request):
 
 def register(request):
     context = RequestContext(request)
+    username = request.POST['username']
+    password = request.POST['password1']
     if request.method == 'POST':
-    	form = UserForm(data=request.POST)
+        form = UserForm(data=request.POST)
+        critic_form = CriticForm()
         if form.is_valid():
             user = form.save()
-            user.set_password(user.password)
+            #user.set_password(user.password)
             user.save()
-            return HttpResponseRedirect('/')
+
+            critic = critic_form.save(commit=False)
+
+            critic.user = user
+            
+            critic.save()
+            user2 = authenticate(username=username, password=password)        
+            if user2 is not None:
+                if user.is_active:
+                    auth_login(request, user2)
+                    return HttpResponseRedirect('/feed')
+                else:
+                    pass
+            else:
+                return HttpResponseRedirect('/')
         else:
             return HttpResponseRedirect('/')
     else:
