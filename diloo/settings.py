e6e9ca60d5053559f8dd9ee03a974ff8f600b821
hugoduar <hugoduar@hotmail.com Default Deletehugoduar@hotmail.com>
2014-05-07 03:17:56 -0500

    deploying to heroku

--- diloo/settings.py
+++ diloo/settings.py
@@ -8,99 +8,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# import os
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# import dj_database_url
-
-
-
-
-# # Quick-start development settings - unsuitable for production
-# # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'c*ke_xvlnlh_-5mu10mabu18-!bq^9lvufbx1*hm&kg)ms46ll'
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-# TEMPLATE_DEBUG = True
-
-# ALLOWED_HOSTS = ['*']
-
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
-# # Application definition
-
-# INSTALLED_APPS = (
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'dilooapp',
-#     'south', 
-#     'dilooapp.templatetags',
-# )
-
-# MIDDLEWARE_CLASSES = (
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# )
-
-# ROOT_URLCONF = 'diloo.urls'
-
-# WSGI_APPLICATION = 'diloo.wsgi.application'
-
-
-# # Database
-# # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "diloodb",
-#         "USER": "root",
-#         "PASSWORD": "n0m3l0",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
-# DATABASES['default'] =  dj_database_url.config()
-
-# # Internationalization
-# # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-# LANGUAGE_CODE = 'en-us'
-
-# TIME_ZONE = 'UTC'
-
-# USE_I18N = True
-
-# USE_L10N = True
-
-# USE_TZ = True
-
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-#     os.path.join(BASE_DIR, 'app/static'),
-# )
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+import dj_database_url
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -116,15 +28,8 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Application definition
 
@@ -132,13 +37,13 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.messages',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'south',
     'app',
+    'south', 
 )
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,11 +58,25 @@ ROOT_URLCONF = 'diloo.urls'
 WSGI_APPLICATION = 'diloo.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "diloodb",
+        "USER": "root",
+        "PASSWORD": "n0m3l0",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'es-mx'
+LANGUAGE_CODE = 'en-mx'
 
 TIME_ZONE = 'UTC'
 
@@ -171,9 +90,89 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'app/static'),
+)
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-TEMPLATE_DIRS = {
-     os.path.join(BASE_DIR, 'app/templates'),
-}
+
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'c*ke_xvlnlh_-5mu10mabu18-!bq^9lvufbx1*hm&kg)ms46ll'
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# TEMPLATE_DEBUG = True
+
+# ALLOWED_HOSTS = ['*']
+
+# # Database
+# # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# # Application definition
+
+# INSTALLED_APPS = (
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.messages',
+#     'django.contrib.sessions',
+#     'django.contrib.staticfiles',
+#     'django.contrib.humanize',
+#     'south',
+#     'app',
+# )
+# MIDDLEWARE_CLASSES = (
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# )
+
+# ROOT_URLCONF = 'diloo.urls'
+
+# WSGI_APPLICATION = 'diloo.wsgi.application'
+
+
+
+# # Internationalization
+# # https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+# LANGUAGE_CODE = 'es-mx'
+
+# TIME_ZONE = 'UTC'
+
+# USE_I18N = True
+
+# USE_L10N = True
+
+# USE_TZ = True
+
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+# STATIC_URL = '/static/'
+
+
+# TEMPLATE_DIRS = {
+#      os.path.join(BASE_DIR, 'app/templates'),
+# }
